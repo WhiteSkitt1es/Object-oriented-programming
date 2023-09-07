c291365b35a35ece16bdc4dd37ffad66e4c84377
Pavel Artem'yev <pasha.artemev48@gmail.com>
2023-09-07 22:37:03 +0300

    Homework completed

---
+++ src/Lesson_work_4/homework/Box.java
@@ -0,0 +1,47 @@
+package Lesson_work_4.homework;
+
+import java.util.ArrayList;
+
+public class Box<T extends Fruit>  {
+
+    private final ArrayList<T> fructs;
+
+    public Box() {
+        this.fructs = new ArrayList<>();
+    }
+    public float getWeightBox(){
+        float result = 0;
+        for (T fruct: fructs) {
+            result += fruct.getWeight();
+        }
+        return result;
+    }
+    public void addFruct(T fruct){
+        fructs.add(fruct);
+    }
+    private T getFruit(){
+        return fructs.get(0);
+    }
+    public int getQuantity(){
+        return fructs.size();
+    }
+    public void isEmpty(){
+        fructs.clear();
+    }
+    public void sprinkle(Box<T> box){
+        for (int i = 0; i < box.getQuantity(); i++) {
+            addFruct(getFruit());
+        }
+        box.isEmpty();
+    }
+
+    public boolean compare(Box box){
+        int res = Float.compare(getQuantity(), box.getQuantity());
+        if (res == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/Lesson_work_4/homework/Program.java
@@ -0,0 +1,60 @@
+package Lesson_work_4.homework;
+
+import java.util.ArrayList;
+
+public class Program {
+
+    /**
+     * a. Даны классы Fruit, Apple extends Fruit, Orange extends Fruit;
+     * b. Класс Box, в который можно складывать фрукты. Коробки условно сортируются по типу фрукта,
+     * поэтому в одну коробку нельзя сложить и яблоки, и апельсины;
+     * c. Для хранения фруктов внутри коробки можно использовать ArrayList;
+     * d. Сделать метод getWeight(), который высчитывает вес коробки, зная вес одного фрукта и их количество:
+     * вес яблока – 1.0f, апельсина – 1.5f (единицы измерения не важны);
+     * e. Внутри класса Box сделать метод compare(), который позволяет сравнить текущую коробку с той, которую
+     * подадут в compare() в качестве параметра. true – если их массы равны, false в противоположном случае.
+     * Можно сравнивать коробки с яблоками и апельсинами;
+     * f. Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую.
+     * Помним про сортировку фруктов: нельзя яблоки высыпать в коробку с апельсинами.
+     * Соответственно, в текущей коробке фруктов не остается, а в другую перекидываются объекты, которые были в первой;
+     * g. Не забываем про метод добавления фрукта в коробку.
+     *
+     * Формат сдачи: ссылка на гитхаб проект
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Box<Apple> boxApple1 = new Box<>();
+        for (int i = 0; i < 5; i++) {
+            boxApple1.addFruct(new Apple());
+        }
+        System.out.printf("Колличество яблок в данной корзине %d, а вес ее составляет %.1f\n", boxApple1.getQuantity(), boxApple1.getWeightBox());
+
+        Box<Apple> boxApple2 = new Box<>();
+        for (int i = 0; i < 5; i++) {
+            boxApple2.addFruct(new Apple());
+        }
+        System.out.printf("Колличество яблок в данной корзине %d, а вес ее составляет %.1f\n", boxApple2.getQuantity(), boxApple2.getWeightBox());
+
+        Box<Orange> boxOrange1 = new Box<>();
+        for (int i = 0; i < 4; i++) {
+            boxOrange1.addFruct(new Orange());
+        }
+        System.out.printf("Колличество апельсинов в данной корзине %d, а вес ее составляет %.1f\n", boxOrange1.getQuantity(), boxOrange1.getWeightBox());
+
+        Box<Orange> boxOrange2 = new Box<>();
+        for (int i = 0; i < 4; i++) {
+            boxOrange2.addFruct(new Orange());
+        }
+        System.out.printf("Колличество апельсинов в данной корзине %d, а вес ее составляет %.1f\n", boxOrange2.getQuantity(), boxOrange2.getWeightBox());
+
+        System.out.println(boxApple1.compare(boxOrange1)); // Сравнили коробки
+
+        boxApple1.sprinkle(boxApple2); // Пересыпали яблоки
+//        boxApple1.sprinkle(boxOrange1); // Пересыпать апельсины в яблоки нельзя
+        System.out.println(boxApple1.getQuantity());
+        System.out.println(boxApple2.getQuantity());
+
+    }
+
+}
