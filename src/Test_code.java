968214edf6862dbf78fc242001d7b0a3a4c7a8e8
Pavel Artem'yev <pasha.artemev48@gmail.com>
2023-09-08 10:31:06 +0300

    Test code add

---
+++ src/Test_code.java
@@ -0,0 +1,8 @@
+public class Test_code {
+    public static void main(String[] args) {
+        int num1 = -8;
+        int num2 = 8;
+        System.out.println(Math.abs(num1));
+        System.out.println(Math.abs(num2));
+    }
+}
